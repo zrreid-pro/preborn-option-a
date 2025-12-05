b4325a56c211144b3e0af4da034cf96d68f504af
Z <zrreid97@gmail.com>
2025-12-05 14:37:56 -0500

    Initial Commit of working blank project.

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,10 @@
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quick Donation API</title>
+</head>
+<body>
+    
+</body>
+</html>
